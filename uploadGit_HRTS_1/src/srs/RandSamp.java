11578dc5468bf98c2d14f6b4081f601ac3e38270
shin <shin@128.16.80.251>
2013-06-18 15:26:04 +0100

    srs sample 10%

--- uploadGit_HRTS_1/src/srs/RandSamp.java
+++ uploadGit_HRTS_1/src/srs/RandSamp.java
@@ -13,9 +13,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.log4j.Logger;
 
 public class RandSamp {
-
+	private static final Logger sLogger = Logger.getLogger(RandSamp.class);
 	public static class RandSampMapper extends
 			Mapper<Object, Text, NullWritable, Text> {
 		private Random rands = new Random();
@@ -40,26 +41,36 @@ public class RandSamp {
 	}//end mapper
 
 	public static void main(String[] args) throws Exception{
+
 		Configuration conf = new Configuration();
 		conf.set("filter_percentage", "10");//shin:sample at 10%
+		
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 		if (otherArgs.length != 2) {
 			System.err.println("Usage: BigramRelativeFrequency <in> <out>");
 			System.exit(2);
 		}
 		int reduceTask = 1;// shin:use 1 reducer
-
+		
+		
 		Job job = new Job(conf, "RandSamp");
 		job.setJarByClass(RandSamp.class);
 		job.setNumReduceTasks(reduceTask);// shin: set number of reducers to 1 for rand samp
 		job.setMapperClass(RandSampMapper.class);
-		job.setOutputKeyClass(Text.class);
+		job.setOutputKeyClass(NullWritable.class);
 		job.setOutputValueClass(Text.class);
-		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputKeyClass(NullWritable.class);
 		job.setMapOutputValueClass(Text.class);
 		FileInputFormat.setInputPaths(job, new Path(otherArgs[0]));// shin
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		long startTime = System.currentTimeMillis();
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
+
+		sLogger.info("Job Finished in "
+				+ (System.currentTimeMillis() - startTime) / 1000.0
+				+ " seconds");
+		
 	}//end main
 
 }//end class
