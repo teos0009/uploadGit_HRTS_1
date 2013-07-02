c42fa5b07e3766fdbb3e0a7d3359aa76135a11ed
shin <shin@ultraman7>
2013-07-03 00:39:10 +0100

    sorted out on chrome's content script for keypress, highlight text and query on clueue, and other methodology that doesnt work.

---
+++ uploadGit_HRTS_1/clue_CS/js/bg.js
@@ -0,0 +1,106 @@
+//console.log("Loaded content script with jQuery for " + window.location.href);
+//document.addEventListener("keydown", keyDownTextField, false);//put here cant capture event
+
+var seltext = null;
+var keypressText = null;
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse)
+{
+	switch(request.message)
+	{
+		case 'setText':
+			window.seltext = request.data
+			console.log("setText: "+seltext);
+		break;
+		
+		case 'keydown1':
+			window.keypressText = request.data
+			rxKeyCode(keypressText);//style2
+			console.log("bg keydown1: "+keypressText);	
+		break;
+		
+		default:
+			sendResponse({data: 'Invalid arguments'});
+		break;
+	}
+});
+
+
+//test keypress handling
+//style2
+function rxKeyCode(keypressText) {
+console.log("rxKeyCode");
+  var kcode = keypressText
+  if(kcode==32) {
+	console.log("You hit the SPACE key.");
+  } else {
+	console.log("You hit OTHER key.");
+  }
+}//end rx key code
+
+/*
+//style1: doesnt work
+document.addEventListener("keydown", keyDownTextField, false);
+function keyDownTextField(event) {
+console.log("inside keydown text field");
+  var kcode = event.keyCode;
+  if(kcode==32) {
+    //alert("You hit the SPACE key.");
+	console.log("You hit the enter key.");
+	event.preventDefault();//If this method is called, the default action of the event will not be triggered.
+  } else {
+    //alert("Oh no you didn't.");
+	console.log("You hit OTHER key.");
+	event.preventDefault();
+  }
+}//end keyDownTextField
+*/
+
+//handling select text and right click context menu
+//use with style1
+function savetext(info,tab)
+{
+	var jax = new XMLHttpRequest();
+	jax.open("POST","http://localhost/text/");
+	//jax.open("POST","http://www.clueue.com/search/?q=asylum+ANY");//no disp in pop up window
+	jax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	jax.send("text="+seltext);
+	jax.onreadystatechange = function() { if(jax.readyState==4) { alert(jax.responseText);	}}
+}//end savetext
+
+//use with style2
+function getword(info,tab) {
+    var any = "+ANY";
+	
+    console.log("Word \"" + info.selectionText + "\" was clicked.");//debug use
+    chrome.tabs.create({ //open a new tab
+        //url: "http://www.google.com/search?q=" + info.selectionText,//default
+		//url: "http://www.clueue.com/search/?q="+ info.selectionText,//no phrase search
+		url: "http://www.clueue.com/search/?q="+ info.selectionText + any,//phrase search
+    })
+}//end get word
+
+//==create the right click context menu
+/*
+//style1 create context menu
+var contexts = ["selection"];
+for (var i = 0; i < contexts.length; i++)
+{
+    
+	var context = contexts[i];
+	chrome.contextMenus.create({
+		"title": "Search localhost", 
+		"contexts":[context], 
+		"onclick": savetext
+		});
+    console.log("inside forloop context is "+context);	
+}
+*/
+
+//style2 create context menu
+chrome.contextMenus.create({
+    title: "Clueue: %s", 
+    contexts:["selection"], 
+    onclick: getword,//call getWord()
+
+});
---
+++ uploadGit_HRTS_1/clue_CS/js/myscript.js
@@ -0,0 +1,64 @@
+//this is a "content script" that is sandbox/rendered by chrome. chk chrome faq
+//this js is injected according to param set at manifest.json
+
+document.addEventListener('mouseup',function(event)
+{
+	var sel = window.getSelection().toString();
+	
+	if(sel.length)
+		chrome.extension.sendRequest({
+		'message':'setText',
+		'data': sel
+		},function(response){})
+})
+
+/*
+//style 1: doesnt work
+document.addEventListener("keydown", keyDownTextField, false);
+
+function keyDownTextField(e) {
+console.log("myscr inside keydown text field");
+  var kcode = e.keyCode;
+  if(kcode==32) {
+    //alert("You hit the SPACE key.");
+	console.log("You hit the enter key.");
+	//event.preventDefault();//If this method is called, the default action of the event will not be triggered.
+  } else {
+    //alert("Oh no you didn't.");
+	console.log("You hit OTHER key.");
+    //event.preventDefault();
+  }
+}//end keyDownTextField
+*/
+
+//document.addEventListener('keydown', function(event) {//ok
+//console.log("keycode: " + event.keyCode);
+//})
+
+
+
+//style2 works
+//console out= bg keydown1: my scr keydown: 32
+document.addEventListener('keydown',function(event)
+{
+    var sel = event.keyCode;
+	
+	//console no display keycode
+	//var kcode = e.keyCode;
+	//if(kcode==32) {
+	//	console.log("You hit the SPACE key.");
+	//	alert("Oh no you SPACE");
+	//} else {
+	//console.log("You hit OTHER key.");
+	//alert("Oh no you OTHER");
+	//}	
+	
+	//var sel = "my scr keydown: " + event.keyCode;//debug only
+	//send to bg.js to process
+	chrome.extension.sendRequest({
+	'message':'keydown1',
+	'data': sel
+	},function(response){})
+
+});
+
